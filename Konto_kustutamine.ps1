6359984bbb33f4a30a7bc14b8bdb41338fd71751
Henry-Heiki <henry-heiki.ein@voco.ee>
2023-01-07 13:30:53 +0200

    Konto kustutamine nime järgi

---
+++ Konto_kustutamine.ps1
@@ -0,0 +1,17 @@
+﻿# Skript kustutab kasutaja tema ees ja perenime põhjal
+Echo "Sisesta eesnimi"
+$eesnimi=Read-Host
+Echo "Sisesta oma perekonnanimi"
+$perenimi=Read-Host
+$kasutajanimi=$eesnimi.ToLower() + "." + $perenimi.ToLower()
+$ErrorActionPreference = "SilentlyContinue"
+Remove-LocalUser "$kasutajanimi"
+if(!$?)
+{
+Echo "Viga! kontot ei ole olemas."
+}
+else
+{
+Echo "Konto $kasutajanimi on kustutatud"
+}
+$ErrorActionPreference = "Stop"
